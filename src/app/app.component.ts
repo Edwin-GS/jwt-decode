41d0bb0c9b0c57042495a27685b1b98318717382
Edwin Guillén Santana <egsant@grupoepelsa.com>
2023-07-27 22:15:33 +0200

    JWT Decoder ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import jwtDecode from "jwt-decode";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+
   title = 'jwt-decode';
+
+  token_decode: string | undefined;
+
+  jwt = {
+    token: ''
+  }
+
+  constructor() {
+  }
+
+  decoder(){
+
+    try {
+
+      this.token_decode = jwtDecode(this.jwt.token);
+
+    } catch {
+
+      this.token_decode = 'Enter a valid token';
+    }
+
+  }
+
 }
